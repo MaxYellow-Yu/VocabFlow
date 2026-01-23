d936e5455e643ab649144c0e99994fdef0a2714b
MaxYellow-Yu <mx3200494857@163.com>
2026-01-23 15:49:34 +0800

    Debug for npm environment

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   // IMPORTANT: This ensures assets (js/css) are looked for relative to index.html
   // instead of at the root of the domain, fixing the blank page issue on GitHub Pages.
-  base: './', 
+  base: './',
+  build: {
+    rollupOptions: {
+      // Tell Rollup that 'xlsx' is external and should not be bundled.
+      // The browser will resolve it using the importmap in index.html.
+      external: ['xlsx']
+    }
+  }
 })
